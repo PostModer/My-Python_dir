e69118f209253e9304ee21e00e4033e2292bea30
MAX L <Maximum00214@yandex.ru>
2021-03-23 23:19:50 +0300

    добавлен Гитигнор и ридми

---
+++ Math_Sol.py
@@ -0,0 +1,10 @@
+import math
+def Math_Sol(x):
+    if x == 0 or x < 0:
+        print("Недопустимое значение x, введите другое!")
+    else:
+        a = math.tan(x)
+        b = math.exp(math.cos(x))
+        y = (a + math.sin(b) ** 2 - math.log(x + 1)) / x
+    return y
+print(Math_Sol(4))
